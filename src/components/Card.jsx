8884dee7865b4eb625c3564ddbb6797573af99a2
Esvicvictor <esvicvi@gmail.com>
2023-07-10 11:34:59 +0100

    fixed navbar

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default class Card extends Component {
     return (
       <div className='card' style={{ backgroundColor: `${color}`}}>
         <div className='cardDetail'>
-          <div className='border h-[50px] w-[50px] flex justify-center items-center rounded-full border-white mx-auto mt-5'>
+          <div className='border h-[50px] w-[50px] flex justify-center items-center rounded-full border-white mx-auto mt-2'>
             <i class='fa-solid fa-calendar-day'></i>
           </div>
           <h1>{title}</h1>
